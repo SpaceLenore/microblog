0a2d2ca46f792259a2ebfe1a5378a550dd3f932a
SpaceLenore <libreterra@protonmail.com>
2019-11-08 12:03:32 +0100

    fix and improve unit tests

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -3,7 +3,6 @@ Contains tests for app.models.User class
 """
 # pylint: disable=redefined-outer-name
 from datetime import datetime, timedelta
-import unittest
 from unittest import mock
 import pytest
 from app.models import User, Post
@@ -49,29 +48,35 @@ def test_avatar(_mock_current_app, user1):
                                  '?d=retro&s=128')
 
 def test_follow(test_app):
-        u1 = User(username='john', email='john@example.com')
-        u2 = User(username='susan', email='susan@example.com')
-        db.session.add(u1)
-        db.session.add(u2)
-        db.session.commit()
-        assert u1.followed.all() == []
-        assert u1.followers.all() == []
+    """
+    Test following a user
+    """
+    u1 = User(username='john', email='john@example.com')
+    u2 = User(username='susan', email='susan@example.com')
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+    assert u1.followed.all() == []
+    assert u1.followers.all() == []
 
-        u1.follow(u2)
-        db.session.commit()
-        assert u1.is_following(u2) == True
-        assert u1.followed.count() == 1
-        assert u1.followed.first().username == 'susan'
-        assert u2.followers.count() == 1
-        assert u2.followers.first().username == 'john'
+    u1.follow(u2)
+    db.session.commit()
+    assert u1.is_following(u2)
+    assert u1.followed.count() == 1
+    assert u1.followed.first().username == 'susan'
+    assert u2.followers.count() == 1
+    assert u2.followers.first().username == 'john'
 
-        u1.unfollow(u2)
-        db.session.commit()
-        assert u1.is_following(u2) == False
-        assert u1.followed.count() == 0
-        assert u2.followers.count() == 0
+    u1.unfollow(u2)
+    db.session.commit()
+    assert not u1.is_following(u2)
+    assert u1.followed.count() == 0
+    assert u2.followers.count() == 0
 
 def test_follow_posts(test_app):
+    """
+    Test follower posts
+    """
     # create four users
     u1 = User(username='john', email='john@example.com')
     u2 = User(username='susan', email='susan@example.com')
